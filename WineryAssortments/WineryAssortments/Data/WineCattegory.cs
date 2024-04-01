26157e24def917aa69b31e42b3251e2fcf730483
Диан Иванов <rijkoladata@gmail.com>
2024-04-01 22:00:51 +0300

    Making a functional promotions controller and a view for it plus some work on the register form

--- WineryAssortments/WineryAssortments/Data/WineCattegory.cs
+++ WineryAssortments/WineryAssortments/Data/WineCattegory.cs
@@ -1,10 +1,14 @@
-﻿namespace WineryAssortments.Data
+﻿using System.ComponentModel;
+
+namespace WineryAssortments.Data
 {
     public class WineCattegory
     {
         public int Id { get; set; }
+        [DisplayName("Име на категорията")]
         public string Name { get; set; }
         public ICollection<Wine> Wines { get; set; }
+        [DisplayName("Дата на промяна")]
         public DateTime DateModified { get; set; } = DateTime.Now;
     }
 }
--- WineryAssortments/WineryAssortments/Data/WineType.cs
+++ WineryAssortments/WineryAssortments/Data/WineType.cs
@@ -1,10 +1,14 @@
-﻿namespace WineryAssortments.Data
+﻿using System.ComponentModel;
+
+namespace WineryAssortments.Data
 {
     public class WineType
     {
         public int Id { get; set; }
+        [DisplayName("Име на вида вино")]
         public string Name { get; set; }
         public ICollection<Wine> Wines { get; set; }
+        [DisplayName("Дата на промяна")]
         public DateTime DateModified { get; set; } = DateTime.Now;
     }
 }
