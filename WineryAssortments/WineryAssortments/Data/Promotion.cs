6960405c55fac94253e34a41a0f5b273a8f45281
Диан Иванов <rijkoladata@gmail.com>
2024-04-14 12:01:04 +0300

    Some work on the design of the gallery and also some work on the promotions index view

--- WineryAssortments/WineryAssortments/Data/Promotion.cs
+++ WineryAssortments/WineryAssortments/Data/Promotion.cs
@@ -1,13 +1,21 @@
-﻿namespace WineryAssortments.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace WineryAssortments.Data
 {
     public class Promotion
     {
         public int Id { get; set; }
         public int WinesId { get; set; }
+        [DisplayName("Промоционално вино")]
         public Wine Wines { get; set; }
+        [DisplayName("Промоция")]
         public int PromotionPercent { get; set; }
+        [DisplayName("Начална дата")]
         public DateTime StartDate { get; set; }
+        [DisplayName("Крайна дата")]
         public DateTime EndDate { get; set; }
+        [DisplayName("Дата на промяна")]
         public DateTime DateModified { get; set; } = DateTime.Now;
     }
 }
